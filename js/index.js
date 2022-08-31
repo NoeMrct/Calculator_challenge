d3206036a0fdd5fa6aaccf0d3b797c23abbe39cc
[Noé <93987930+NoeMrct@users.noreply.github.com>
2022-08-31 12:20:02 +0200

    second commit

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const display = document.querySelector(".first_text");
 let secondText = document.querySelector(".second_text");
 let thirdText = document.querySelector(".third_text");
 let firstValue = null;
-let secondValue = null;
+let secondValue = "";
 let operator = null;
 let results = null;
 
@@ -11,68 +11,91 @@ function buttonClick(e) {
   const key = e;
   const action = key.getAttribute("data-action");
   const keyContent = key.textContent;
-  const displayedNum = display.textContent;
+  let displayedNum = display.textContent;
   if (!action) {
     if (!operator) {
-      if (displayedNum === "0") {
-        display.textContent = parseFloat(keyContent);
-      } else if (results) {
-        display.textContent = parseFloat(keyContent);
-        if (secondText.textContent == null) {
-          secondText.textContent = results;
-        } else {
-          thirdText.textContent = secondText.textContent;
-          secondText.textContent = results;
-        }
+      if (results) {
+        thirdText.textContent = secondText.textContent;
+        secondText.textContent = "= " + displayedNum;
         firstValue = null;
-        secondValue = null;
         operator = null;
         results = null;
+        secondValue = "";
+        display.textContent = "0";
+        displayedNum = display.textContent;
+      }
+      if (displayedNum === "0") {
+        display.textContent = parseFloat(keyContent);
       } else {
         display.textContent = displayedNum + parseFloat(keyContent);
       }
       firstValue = displayedNum;
     } else {
-      if (displayedNum === "0") {
-        display.textContent = parseFloat(keyContent);
-      } else if (results) {
-        display.textContent = parseFloat(keyContent);
-        if (secondText.textContent == null) {
-          secondText.textContent = results;
-        } else {
-          thirdText.textContent = secondText.textContent;
-          secondText.textContent = results;
-        }
-        firstValue = null;
-        secondValue = null;
-        operator = null;
-        results = null;
-      } else {
-        display.textContent = displayedNum + parseFloat(keyContent);
-      }
+      display.textContent = displayedNum + parseFloat(keyContent);
+      let value = parseFloat(keyContent);
+      secondValue += value.toString();
     }
-  } else if (
-    action === "add" ||
-    action === "subtract" ||
-    action === "multiply" ||
-    action === "divide"
-  ) {
+  } else if (action === "add") {
+    if (results) {
+      thirdText.textContent = secondText.textContent;
+      secondText.textContent = "= " + displayedNum;
+    }
     firstValue = displayedNum;
     operator = action;
-    display.textContent = 0;
+    display.textContent = firstValue + " + ";
+  } else if (action === "subtract") {
+    if (results) {
+      thirdText.textContent = secondText.textContent;
+      secondText.textContent = "= " + displayedNum;
+    }
+    firstValue = displayedNum;
+    operator = action;
+    display.textContent = firstValue + " - ";
+  } else if (action === "multiply") {
+    if (results) {
+      thirdText.textContent = secondText.textContent;
+      secondText.textContent = "= " + displayedNum;
+    }
+    firstValue = displayedNum;
+    operator = action;
+    display.textContent = firstValue + " * ";
+  } else if (action === "divide") {
+    if (results) {
+      thirdText.textContent = secondText.textContent;
+      secondText.textContent = "= " + displayedNum;
+    }
+    firstValue = displayedNum;
+    operator = action;
+    display.textContent = firstValue + " / ";
   } else if (action === "decimal") {
     display.textContent = displayedNum + ".";
   } else if (action === "pourcentage") {
     display.textContent = displayedNum / 100;
   } else if (action === "clear") {
+    firstValue = null;
+    secondValue = "";
+    operator = null;
+    results = null;
     secondText.textContent = thirdText.textContent = null;
     display.textContent = 0;
   } else if (action === "delete") {
+    if (results) {
+      firstValue = null;
+      secondValue = "";
+      operator = null;
+      results = null;
+      thirdText.textContent = "";
+      secondText.textContent = "";
+    }
     display.textContent = displayedNum.slice(0, -1);
   } else if (action === "calculate") {
-    secondValue = displayedNum;
     results = calculate(firstValue, operator, secondValue);
+    thirdText.textContent = "";
+    secondText.textContent = displayedNum;
     display.textContent = results;
+    firstValue = null;
+    secondValue = "";
+    operator = null;
   }
 }
 
